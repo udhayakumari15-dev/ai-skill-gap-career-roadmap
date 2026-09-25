92f58cf40774c4d3415ca1f53b3b7a50cb5e0aab
A. Udhaya Kumari <kumariudhaya15@gmail.com>
2026-09-25 15:41:49 +0530

    Redesign UI and update career features

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect 
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
