940138242453b28db59e9402daa93cc4f6c0a8d7
A. Udhaya Kumari <kumariudhaya15@gmail.com>
2026-09-25 16:00:56 +0530

    Prepare Django project for deployment

--- project/settings.py
+++ project/settings.py
@@ -20,10 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1z1*gotdu#dvgp_6edu!_&=g13e+=r34t$cq%e@xntty)kc6=5'
+import os
+
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-local-development-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
